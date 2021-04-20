8551cdd103ac84867f458f8871ffa8f3bb15e6de
momnaazhar <80837855+momnaazhar@users.noreply.github.com>
2021-04-20 11:00:08 +1000

    Update Q2b.sql

--- Q2b.sql
+++ Q2b.sql
@@ -1,1 +1,1 @@
-select LastName, FirstName, MemberID, JoinDate from Member where JoinDate > '2012-12-31' and JoinDate < '2014-01-01';
+select LastName, FirstName, MemberID from Member where JoinDate > '2012-12-31' and JoinDate < '2014-01-01';
